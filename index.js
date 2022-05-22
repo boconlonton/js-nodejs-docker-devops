6bd1c4b9a7244f1e645dec3f29466b3bb230bfdf
bocon <boconlonton111@gmail.com>
2022-05-22 12:22:18 +0700

    Add - mongodb to the stack

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect("mongodb://root:password@mongo:27017/?authSource=admin")
+  .then(() => console.log("successfully connected to DB"))
+  .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
   res.send("<h2>Hi there bocon</h2>");
