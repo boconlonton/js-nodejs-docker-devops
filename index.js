56a1ab52501127dedd004ed2dcc8ba2c4df8d43d
bocon <boconlonton111@gmail.com>
2022-05-22 12:59:22 +0700

    Add - Post service

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const {
   MONGO_PORT,
 } = require("./config/config");
 
+const postRouter = require("./routes/postRoutes");
+
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
@@ -27,9 +29,14 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+// Middleware for parsing Request Body
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("<h2>Hi there bocon</h2>");
 });
+
+app.use("/api/v1/posts", postRouter);
 
 const port = process.env.PORT || 3000;
 
