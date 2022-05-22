0a6817b83567614c7333a1ea59f499ffa5865ca3
bocon <boconlonton111@gmail.com>
2022-05-22 13:20:11 +0700

    Update - User Sign up - Add hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,15 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
 
 exports.signUp = async (req, res, next) => {
+  const { username, password } = req.body;
+  // Hash password
+  const hashPassword = await bcrypt.hash(password, 12);
   try {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+      username: username,
+      password: hashPassword,
+    });
     res.status(201).json({
       status: "success",
       data: {
@@ -10,6 +17,7 @@ exports.signUp = async (req, res, next) => {
       },
     });
   } catch (e) {
+    console.log(e);
     res.status(400).json({
       status: "failed",
     });
